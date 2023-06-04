66e4e4f43e33933af54d7d0ab47abb484b16bbbd
Gabriela-Ortega-Jacobe <gabrielaortega@byui.edu>
2023-06-03 22:21:57 -0300

    sh

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,37 +20,3 @@ if (weekday === 1 || weekday === 2){
     article.setAttribute('class', 'banner-message');
 }
 
-// select all data-src
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-// select each image, assign the data-src content to src and remove data-src
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
-};
-
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 100px 0px"
-
-};
-
-if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if (item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  };
