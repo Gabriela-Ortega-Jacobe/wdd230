601783278d6bb72ff0ca156a82b6dc2d5e0f383a
Gabriela-Ortega-Jacobe <gabrielaortega@byui.edu>
2023-07-05 18:42:55 -0300

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,40 +1,41 @@
-const url = "https://gabriela-ortega-jacobe.github.io/wdd230/chamber/data.json";
+const url = "https://gabriela-ortega-jacobe.github.io/wdd230/chamber/data.json"
 
-getcompaniesData(url);
-
-async function getcompaniesData(url) {
-  const response = await fetch(url);
-
-  if (response.ok) {
-    const data = await response.json();
-    console.log(data.companies);
-    displaycompanies(data.companies);
-  }
+async function getbussInfo(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const buss = await response.json();
+        displaybuss(buss.buss);
+    }
 }
-const displayBusiness = (companies) => {
-  const cards = document.querySelector('companiescards'); 
-  companies.forEach(companies) => {
-      
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let images = document.createElement('img');
-      let address = document.createElement ('p')
+
+getbussInfo(url);
+
+const displaybuss = (buss) => {
+  const cards = document.querySelector('div.cards');
   
-      
-      h2.textContent = `${companies.name} ____________`;
-  
-      
-      images.setAttribute('src', companies.images);
-      images.setAttribute('alt', `${companies.name} ______________`);
-      images.setAttribute('loading', 'lazy');
-      images.setAttribute('width', '340');
-      images.setAttribute('height', 'auto');
-  
+  prophet.forEach((buss) => {
     
-      card.appendChild(h2);
-      card.appendChild(images);
-      card.appendChild(address);
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birth = document.createElement('p');
+    let portrait = document.createElement('img');
+    
   
-      cards.appendChild(card);
-    }); 
-  } 
+    h2.textContent = `${buss.name}`;
+    
+    portrait.setAttribute('src', buss.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${buss.name}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '300');
+    portrait.setAttribute('height', 'auto');
+    
+   
+    card.appendChild(h2);
+    card.appendChild(birth);
+    card.appendChild(portrait);
+    
+    
+    cards.appendChild(card);
+  });
+
+}
--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -20,3 +20,32 @@ if (weekday === 1 || weekday === 2){
     article.setAttribute('class', 'banner-message');
 }
 
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4a82527f0225aef2a205977175101c88";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+    const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute("src", iconSrc);
+    weatherIcon.setAttribute("alt", desc);
+    capDescrip.textContent = desc;
+}
